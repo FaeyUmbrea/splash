2210f92c3fbe4db71c35fcc093770b177b9ab519
Faey Umbrea <1144986+FaeyUmbrea@users.noreply.github.com>
2023-09-25 14:56:29 +0200

    Configure module name

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,12 @@ import { transform } from "esbuild";
 
 // For convenience, you just need to modify the package ID below as it is used to fill in default proxy settings for
 // the dev server.
-const s_PACKAGE_ID = "modules/boilerplate";
+const s_PACKAGE_ID = "modules/splash";
 
 // A short additional string to add to Svelte CSS hash values to make yours unique. This reduces the amount of
 // duplicated framework CSS overlap between many TRL packages enabled on Foundry VTT at the same time. 'ese' is chosen
 // by shortening 'essential-svelte-esm'.
-const s_SVELTE_HASH_ID = "boilerplate";
+const s_SVELTE_HASH_ID = "splsh";
 
 const s_TERSER = false; // Set to true to use terser
 const s_SOURCEMAPS = true; // Generate sourcemaps for the bundle (recommended).
